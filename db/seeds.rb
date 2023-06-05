24c93aa1b3354c554e177fadb1fa8ad6b035915b
EC2 Default User <129721056+yudai-iwai@users.noreply.github.com>
2023-06-05 22:49:22 +0000

    [registration,session]

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Admin用のデータ
+Admin.create(email: "admin@co.jp", password: "wankomeshi", password_confirmation: "wankomeshi")
