a6935d503a9979ac38937daca633ddf15f3d5116
EC2 Default User <129721056+yudai-iwai@users.noreply.github.com>
2023-06-06 05:31:21 +0000

    [registration]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :dog_name, :dog_breed, :dog_age, :dog_gender])
   end
 end
