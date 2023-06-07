043aad9b338a3e19534536a98478dcd90aa6c37d
EC2 Default User <129721056+yudai-iwai@users.noreply.github.com>
2023-06-07 22:49:44 +0000

    [マイページ、会員一覧・詳細]

--- app/controllers/public/members_controller.rb
+++ app/controllers/public/members_controller.rb
@@ -1,24 +1,38 @@
 class Public::MembersController < ApplicationController
   def index
+    @members = Member.all
   end
 
   def show
+    @member = Member.find(params[:id])
+    @post_recipes = @member.post_recipes
   end
-  
+
   def my_page
     @member = current_member
     @post_recipes = current_member.post_recipes
   end
-  
+
   def edit
+    @member = current_member
   end
-  
+
   def update
+    @member = current_member
+    @member.update(member_params)
+    redirect_to my_page_members_path
   end
-  
+
   def quit
   end
-  
+
   def out
   end
+
+  private
+
+  def member_params
+    params.require(:member).permit(:name, :email, :dog_name, :dog_breed, :dog_age, :dog_gender, :image)
+  end
+
 end
