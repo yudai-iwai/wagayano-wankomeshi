3d21f0d6ffb4f649ea4897a903eaaf9ca3ad30ba
EC2 Default User <129721056+yudai-iwai@users.noreply.github.com>
2023-06-08 07:20:50 +0000

    [いいね機能実装]

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,7 +1,16 @@
 class Public::FavoritesController < ApplicationController
   def create
+    post_recipe = PostRecipe.find(params[:post_recipe_id])
+    favorite = current_member.favorites.new(post_recipe_id: post_recipe.id)
+    favorite.save
+    redirect_to post_recipe_path(post_recipe)
   end
 
   def destroy
+    post_recipe = PostRecipe.find(params[:post_recipe_id])
+    favorite = current_member.favorites.find_by(post_recipe_id: post_recipe.id)
+    favorite.destroy
+    redirect_to post_recipe_path(post_recipe)
   end
+  
 end
