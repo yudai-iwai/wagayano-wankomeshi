3b7656513b2e0ab5f4a91a537acd6fdb768c1c14
EC2 Default User <129721056+yudai-iwai@users.noreply.github.com>
2023-06-16 15:18:14 +0000

    [コメント機能非同期通信（修正）]

--- app/controllers/admin/post_comments_controller.rb
+++ app/controllers/admin/post_comments_controller.rb
@@ -1,8 +1,8 @@
 class Admin::PostCommentsController < ApplicationController
   before_action :authenticate_admin!
+  
   def destroy
+    @post_recipe = PostRecipe.find(params[:post_recipe_id])
     PostComment.find(params[:id]).destroy
-    flash[:notice] = "コメントの削除が成功しました！"
-    redirect_to admin_post_recipe_path(params[:post_recipe_id])
   end
 end
--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -6,6 +6,7 @@ class Public::PostCommentsController < ApplicationController
     comment = current_member.post_comments.new(post_comment_params)
     comment.post_recipe_id = post_recipe.id
     comment.save
+    flash[:notice] = "コメントの送信がしました！"
     @post_recipe = PostRecipe.find(params[:post_recipe_id])
     @post_comment = PostComment.new
   end
@@ -14,6 +15,7 @@ class Public::PostCommentsController < ApplicationController
     @post_recipe = PostRecipe.find(params[:post_recipe_id])
     @post_comment = PostComment.find(params[:id])
     @post_comment.destroy
+    flash[:notice] = "コメントの削除が成功しました！"
     @post_comment = PostComment.new
   end
   
