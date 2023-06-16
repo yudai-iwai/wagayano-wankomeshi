71fa5980043da072b2c59c81d843aee07f38e40c
EC2 Default User <129721056+yudai-iwai@users.noreply.github.com>
2023-06-16 07:12:36 +0000

    [いいね非同期通信]

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,17 +1,15 @@
 class Public::FavoritesController < ApplicationController
   before_action :authenticate_member!
   def create
-    post_recipe = PostRecipe.find(params[:post_recipe_id])
-    favorite = current_member.favorites.new(post_recipe_id: post_recipe.id)
+    @post_recipe = PostRecipe.find(params[:post_recipe_id])
+    favorite = current_member.favorites.new(post_recipe_id: @post_recipe.id)
     favorite.save
-    redirect_to request.referer
   end
 
   def destroy
-    post_recipe = PostRecipe.find(params[:post_recipe_id])
-    favorite = current_member.favorites.find_by(post_recipe_id: post_recipe.id)
+    @post_recipe = PostRecipe.find(params[:post_recipe_id])
+    favorite = current_member.favorites.find_by(post_recipe_id: @post_recipe.id)
     favorite.destroy
-    redirect_to request.referer
   end
 
 end
