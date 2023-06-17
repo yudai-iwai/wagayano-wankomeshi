14ce460aef13a3f248417e1e8f37371e083ca552
EC2 Default User <129721056+yudai-iwai@users.noreply.github.com>
2023-06-17 08:56:30 +0000

    [レイアウト修正}

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -6,7 +6,6 @@ class Public::PostCommentsController < ApplicationController
     comment = current_member.post_comments.new(post_comment_params)
     comment.post_recipe_id = post_recipe.id
     comment.save
-    flash[:notice] = "コメントの送信がしました！"
     @post_recipe = PostRecipe.find(params[:post_recipe_id])
     @post_comment = PostComment.new
   end
@@ -15,7 +14,6 @@ class Public::PostCommentsController < ApplicationController
     @post_recipe = PostRecipe.find(params[:post_recipe_id])
     @post_comment = PostComment.find(params[:id])
     @post_comment.destroy
-    flash[:notice] = "コメントの削除が成功しました！"
     @post_comment = PostComment.new
   end
   
