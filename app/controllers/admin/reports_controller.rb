eddad03e734e23745415fc84925dcc08405af4e1
EC2 Default User <129721056+yudai-iwai@users.noreply.github.com>
2023-06-24 02:58:05 +0000

    [レイアウト修正]

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -1,6 +1,6 @@
 class Admin::ReportsController < ApplicationController
   def index
-    @reports = Report.all
+    @reports = Report.order("id DESC").page(params[:page]).per(10)
   end
 
   def show
